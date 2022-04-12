f57c3e17e01b7a127e35b00c246f2232d1dd6c4e
Daniel Helbingen <danielhelb@hotmail.com>
2022-04-12 20:21:27 -0300

    commint final

---
+++ src/mooks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 0,
+    name: "Daniel"
+  },  
+  {
+    id: 1,
+    name: "Douglas"
+  },  
+  {
+    id: 2,
+    name: "Rafael"
+  },  
+  {
+    id: 3,
+    name: "Ricardo"
+  }
+]
